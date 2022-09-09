e3dc2da235c9d9cedd67539225fad1565ba82844
usrtax <i@user.tax>
2022-09-09 14:29:59 +0800

    🔹

--- dist.sh
+++ dist.sh
@@ -11,12 +11,13 @@ git pull
 version=$(cat package.json|jq -r '.version')
 
 ./build.sh
+cd lib
 min(){
 bun run uglifyjs -- -o min.$2 --compress --mangle --toplevel -- $1.$2
 }
-min lib/index js
-min lib/index cjs
-#exit 0
+min index js
+min index cjs
+cd $_DIR
 mdi
 npm set unsafe-perm true
 npm version patch
